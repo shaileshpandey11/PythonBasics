05a4ba40f0202e4886fa83ed8c96da0b62ffd0dc
Shailesh Pandey <shailesh11july@gmail.com>
2018-04-14 21:38:35 +0530

    Update with string formatting

--- com/shailesh/custom/loop/password_checker.py
+++ com/shailesh/custom/loop/password_checker.py
@@ -1,8 +1,13 @@
 correct_password = "jvm"
 
+firstname = input("Enter first name :")
+lastname = input("Enter last name :")
+
 password = input("Enter password :\n")
 
 while correct_password != password:
     password = input("Wrong password! Please check caps lock button. Enter again \n")
 
-print("Successfully Logged in")
+message = "Hello %s %s, you are logged in" % (firstname,lastname)
+
+print(message)
